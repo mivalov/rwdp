0cbecc41ec36e91121a972c6e4f189787a2e5045
Ccalypso0 <invalide53@gmail.com>
2022-04-27 05:52:09 +0300

    Improve folder structure/webpage design

--- rwdp/main.py
+++ rwdp/main.py
@@ -3,9 +3,25 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('pages/about.html')
+
+@app.route('/tribute-page')
+def tributePage():
+    return render_template('pages/tribute-page.html')
+
+@app.route('/survey-form')
+def surveyForm():
+    return render_template('pages/survey-form.html')
+
+@app.route('/product-landing-page')
+def productLandingPage():
+    return render_template('pages/product-landing-page.html')
+
+@app.route('/technical-documentation-page')
+def technicalDocumentationPage():
+    return render_template('pages/technical-documentation-page.html')
