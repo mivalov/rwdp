998666dc50a1b231e07bea2ba8b3339301b56006
Miroslav Valov <miro.v96@gmail.com>
2022-07-10 00:08:58 +0200

    Update main.py w.r.t PEP8

--- rwdp/main.py
+++ rwdp/main.py
@@ -2,26 +2,32 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/about')
 def about():
     return render_template('pages/about.html')
 
+
 @app.route('/tribute-page')
-def tributePage():
+def tribute_page():
     return render_template('pages/tribute-page.html')
 
+
 @app.route('/survey-form')
-def surveyForm():
+def survey_form():
     return render_template('pages/survey-form.html')
 
+
 @app.route('/product-landing-page')
-def productLandingPage():
+def product_landing_page():
     return render_template('pages/product-landing-page.html')
 
+
 @app.route('/technical-documentation-page')
-def technicalDocumentationPage():
+def technical_documentation_page():
     return render_template('pages/technical-documentation-page.html')
